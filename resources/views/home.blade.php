a677fcf5ab0196e84c725d6967eb5d0af08c9491
AliSarfraz55 <alisarfrazz8250@gmail.com>
2026-08-17 16:57:48 +0500

    Update website

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome to Sublime School System</title>
-    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
-<link rel="shortcut icon" type="image/png" href="{{ asset('favicon.png') }}">
+   <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+    <link rel="shortcut icon" href="{{ asset('favicon.png') }}" type="image/png">
+    <link rel="apple-touch-icon" href="{{ asset('favicon.png') }}">
     <link rel="stylesheet" href="{{ asset('style.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css">
